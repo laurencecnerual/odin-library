da02d23977b41e33a65a5273e9885b26a9a91b30
laurencecnerual <47619680+laurencecnerual@users.noreply.github.com>
2024-07-10 15:40:08 +0900

    Adds collapsed sidebar and button to add Book
    Also moved DOM adding behavior to addBookToLibrary

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const myLibrary = [];
-const body = document.querySelector("body");
+const wholepage = document.querySelector("body");
+const sidebar = document.querySelector("aside");
+const mainbody = document.querySelector("main");
+const sideButton = document.querySelector("button.sidebar");
+
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -20,14 +24,33 @@ function Book(title, author, pages, read) {
 
 function addBookToLibrary(book) {
     myLibrary.push(book);
+    let myBook = document.createElement("div");
+    myBook.classList.add("book");
+    myBook.setAttribute("id", myLibrary.length + 1);
+    myBook.textContent = myLibrary[myLibrary.length - 1].info();
+    mainbody.appendChild(myBook);
+}
+
+sideButton.addEventListener("click", () => {
+    addBookToLibrary(new Book(`The Hobbit XYZ`, "J.R.R. Tolkien", 3000, false));
+    console.log(myLibrary);
+});
+
+function removeBookFromLibrary(book) {
+    myLibrary.pop(book);
+}
+
+function toggleReadStatus(book) {
+    book.read *= !book.read;
 }
 
 function displayLibrary() {
     for (let key in myLibrary) {
         let singleBook = document.createElement("div");
         singleBook.classList.add("book");
+        singleBook.setAttribute("id", Number(key) + 1);
         singleBook.textContent = myLibrary[key].info();
-        body.appendChild(singleBook);
+        mainbody.appendChild(singleBook);
     }
 }
 
@@ -35,4 +58,14 @@ for (let i = 1; i < 30; i++) {
     addBookToLibrary(new Book(`The Hobbit ${i}`, "J.R.R. Tolkien", 290 + i, i % 2 ? true : false));
 }
 
-displayLibrary();
+// for (let key in myLibrary) {
+//     if ((Number(key) + 1) % 3) {
+//         removeBookFromLibrary(myLibrary[key]);
+//     }
+// }
+
+// for (let key in myLibrary) {
+//     if ((Number(key) + 1) % 5) {
+//         toggleReadStatus(myLibrary[key]);
+//     }
+// }
